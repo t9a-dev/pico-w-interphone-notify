11a6e60288ad623aac7494c3ce8f2d173f9558bc
t9a <t9a.dev@icloud.com>
2026-03-22 09:58:03 +0900

    initial commit

---
+++ src/apiClient.py
@@ -0,0 +1,37 @@
+import os
+import ssl
+import microcontroller
+import adafruit_requests
+import base64
+
+class notify:
+  def __init__(self,socket):
+    self.client = adafruit_requests.Session(socket,ssl.create_default_context())
+  
+  def requestTest(self):
+    print('info: requestTest')
+    try:
+      self.client.request(method='GET',url = 'https://httpbin.org/get');
+    except:
+      # 疎通確認できなければ再起動(ハードリセット)してシステムの回復を試みる。(起動時にネットワーク接続を行っているので)
+      # https://learn.adafruit.com/circuitpython-essentials/circuitpython-resetting#hard-reset-3087083
+      microcontroller.reset();
+
+  def pushToNtfy(self,url,message):
+    ntfy_auth_value = os.getenv('NTFY_AUTH_VALUE')
+    encode_credentials = base64.b64encode(ntfy_auth_value.encode('utf-8')).decode('utf-8')
+    print(encode_credentials)
+    headers = {
+        'Authorization':f'Basic {encode_credentials}',
+    }
+    data = message
+    print('info: pushToNotify')
+    try:
+      self.client.request(
+        method='POST',
+        url=url,
+        headers=headers,
+        data = data,
+      )
+    except:
+      pass
---
+++ src/code.py
@@ -0,0 +1,42 @@
+# screen /dev/tty.usb_name 115200
+import os
+import board
+import analogio
+import time
+import setupWifi
+import apiClient
+
+socket = setupWifi.run()
+notify = apiClient.notify(socket)
+loop_count = 0
+sensor_val = 0
+connection_test_interval = 0
+
+# ADC2(Raspberrypi Pico W GPIOピン34)の値を取得
+adc2 = analogio.AnalogIn(board.A2)
+ntfy_endpoint = os.getenv('NTFY_ENDPOINT')
+notify_sensor_threshold = os.getenv('NOTIFY_SENSOR_THRESHOLD')
+
+# 起動時に一度LINEに通知を送る。(正しくネットワークに接続していることを確認するため)
+notify.pushToNtfy(f'{ntfy_endpoint}/test','Raspberry Pi Pico Wが起動しました。')
+
+def get_voltage(pin):
+    return (pin.value * 3.3) / 65536
+
+
+while True:
+    if connection_test_interval > 60:
+        notify.requestTest()
+        connection_test_interval = 0
+        loop_count = 0
+
+    connection_test_interval = connection_test_interval + 1
+    time.sleep(1)
+    # 光センサーの値。センサーを光で照らしたり手で覆ったりすると値が変わるのでこの値を参考にしてsettings.tomlのNOTIFY_SENSOR_THRESHOLDを調整すること。
+    sensor_val = get_voltage(adc2)
+    print(sensor_val)
+    if sensor_val > notify_sensor_threshold:
+        notify.pushToNtfy(f'{ntfy_endpoint}/bell','インターホンが押されました')
+    loop_count = loop_count + 1
+    print('loop_count: ')
+    print(loop_count)
